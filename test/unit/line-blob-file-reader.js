c45ea53021816f1f71d9cec10b1a44260ab68772
Kevin Kanzelmeyer <kanzelm4@gmail.com>
2016-08-11 20:50:13 -0500

    removed old test methods

--- test/unit/line-blob-file-reader.js
+++ test/unit/line-blob-file-reader.js
@@ -1,18 +1,4 @@
 import lineBlobFileReader from '../../src/line-blob-file-reader';
 
 describe('lineBlobFileReader', () => {
-  describe('Greet function', () => {
-    beforeEach(() => {
-      spy(lineBlobFileReader, 'greet');
-      lineBlobFileReader.greet();
-    });
-
-    it('should have been run once', () => {
-      expect(lineBlobFileReader.greet).to.have.been.calledOnce;
-    });
-
-    it('should have always returned hello', () => {
-      expect(lineBlobFileReader.greet).to.have.always.returned('hello');
-    });
-  });
 });
